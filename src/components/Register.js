20e398a345583c414841269c96c6a8ce54b88264
John Patrick Resurreccion <patrickjohn.resurreccion18@gmail.com>
2022-04-19 14:42:22 +0800

    add email validator

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,15 @@
+import React, { useState } from "react";
 import axios from "axios";
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = ({ baseUri, user, setUser }) => {
-  const handleSubmit = (e) => {
+  const [status, setStatus] = useState();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const newUser = {
       name: e.target.name.value,
@@ -10,12 +17,32 @@ const Register = ({ baseUri, user, setUser }) => {
       password: e.target.password.value,
     };
 
-    axios
+    // const post = await axios.post(`${baseUri}/register`, newUser);
+    // const res = await post;
+    // console.log(res);
+
+    await axios
       .post(`${baseUri}/register`, newUser)
-      .then((res) => setUser([...user, res.data]))
-      .catch((err) => console.log(err));
+      .then((res) => {
+        console.log(res);
+        setUser([...user, res.data]);
+        alert("Register successfully");
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err.message);
+        if (err) {
+          setStatus(409);
+        } else {
+          setStatus("");
+        }
+        setEmail("");
+      });
   };
+
   console.log(user);
+  console.log(status);
+
   return (
     <form
       className=" shadow-lg rounded bg-white text-neutral-900 p-4 space-y-2 "
@@ -28,15 +55,22 @@ const Register = ({ baseUri, user, setUser }) => {
         placeholder="Name"
         name="name"
         required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
         className=" border-[1px] border-neutral-400 py-2 px-2 outline-none focus:border-amber-700 rounded w-[230px] "
       />
       <br />
       <input
         type="email"
-        placeholder="Email"
+        placeholder={status === 409 ? "Email already used!" : "Email"}
         name="email"
         required
-        className=" border-[1px] border-neutral-400 py-2 px-2 outline-none focus:border-amber-700 rounded w-[230px] "
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className={
+          " border-[1px] py-2 px-2 border-neutral-400 outline-none focus:border-amber-700 rounded w-[230px] " +
+          (status === 409 ? " outline-red-700 outline-[1px]" : " outline-none")
+        }
       />
       <br />
       <input
@@ -44,6 +78,8 @@ const Register = ({ baseUri, user, setUser }) => {
         placeholder="Password"
         name="password"
         required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         className=" border-[1px] border-neutral-400 py-2 px-2 outline-none focus:border-amber-700 rounded w-[230px] "
       />
       <br />
