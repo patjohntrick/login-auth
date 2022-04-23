1666205b9eaa93c9f1bd630eea86c66d62f7b1fb
John Patrick Resurreccion <patrickjohn.resurreccion18@gmail.com>
2022-04-23 09:40:46 +0800

    thanks G polyfill has fixed

--- src/components/Logged.js
+++ src/components/Logged.js
@@ -1,32 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { Link, Navigate } from "react-router-dom";
-// import jwt from "jsonwebtoken";
+import { useNavigate } from "react-router-dom";
+import jwt from "jsonwebtoken";
 
 const Logged = () => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-  //   if (token) {
-  //     // const auth = jwt.decode(token);
-  //     // console.log(auth);
-  //   } else {
-  //     alert("Please login");
-  //     // Navigate("/login");
-  //   }
-  // }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      // const auth = jwt.decode(token);
+      // console.log(auth);
+      const auth = jwt.decode(token);
+      console.log(auth);
+      if (!auth) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      } else {
+        setUser(auth);
+      }
+    } else {
+      alert("Please login");
+      navigate("/login");
+    }
+  }, []);
+  const logOut = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+  console.log(user);
   return (
     <div className=" bg-white shadow-lg p-4 rounded ">
       <p className=" text-2xl text-center capitalize ">
-        welcome<span className=" font-bold "> name!!!</span>
+        welcome<span className=" font-bold "> {user.name}</span>
       </p>
-      <Link to="/login">
+      <button
+        className=" bg-sky-700 hover:bg-sky-800 text-sm font-bold uppercase px-4 py-2 rounded mt-4 text-white "
+        onClick={logOut}
+      >
         {" "}
-        <button className=" bg-sky-700 hover:bg-sky-800 text-sm font-bold uppercase px-4 py-2 rounded mt-4 text-white ">
-          {" "}
-          logout{" "}
-        </button>{" "}
-      </Link>
+        logout{" "}
+      </button>{" "}
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ module.exports = {
   resolve: {
     fallback: {
       buffer: require.resolve("buffer/"),
-      crypto: require.resolve("crypto/browserify"),
       util: require.resolve("util/"),
-      stream: require.resolve("stream-browserify"),
       process: require.resolve("process/browser"),
+      stream: require.resolve("stream-browserify"),
+      crypto: require.resolve("crypto-browserify"),
     },
   },
 };
